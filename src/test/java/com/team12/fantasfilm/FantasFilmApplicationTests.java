74ef7941000892b8be8b8b3a7fd8e051dc931b6e
HelloWorld <longnguyenthanh05.12.02@gmail.com>
2024-05-10 20:53:46 +0700

    init

---
+++ src/test/java/com/team12/fantasfilm/FantasFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team12.fantasfilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasFilmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
