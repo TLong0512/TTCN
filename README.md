743bb9b953c9a503849d67e611d952a6e518bf3e
Long Nguyen Thanh <132772325+TLong0512@users.noreply.github.com>
2024-06-02 21:17:32 +0700

    del all

--- README.md
+++
@@ -1,2 +0,0 @@
-
-vào src/main/resource/ file .properties thay đổi url name password của mng nhá
--- src/main/java/com/team12/fantafilm/service/IUserService.java
+++
@@ -1,8 +0,0 @@
-package com.team12.fantafilm.service;
-
-import com.team12.fantafilm.model.User;
-
-public interface IUserService {
-     User findByUserName(String userName);
-
-}
